c63809d6ce785f4006ab556954159f68d08be2a5
ninjafiveo <mjsekol@live.com>
2024-08-21 12:23:24 -0400

    Add project files.

---
+++ C_Sharp_Basics_2425/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basics_2425
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void Comments()
+        {
+            Console.WriteLine("Comments have slashes.");
+            // This is a comment
+            /*This
+             * is
+             * a
+             * multiline
+             * comment
+             */
+            Console.WriteLine("Multiline Comments start and end with /* */");
+        }
+
+    }
+}
